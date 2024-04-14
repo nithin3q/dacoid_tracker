9eb65aacc75be3631f647645a337c9d26f8c2fbc
Nithin Kumar Appari <52022957+nithin3q@users.noreply.github.com>
2024-04-14 10:18:10 +0530

    Update SecondPage.jsx

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -13,8 +13,8 @@ function SecondPage() {
         <Link to="/signup"><p className="absolute top-2 right-0 h-16 w-12  text-blue-500 underline">Skip</p></Link>
         <img src={image} alt="random image" className="mx-auto mt-20 w-100" />
         <p className="text-xl font-medium mt-14 mb-3">Get Burn</p>
-        <p className="text-gray-400">Let’s keep burning to achieve your goals, it hurts only temporarily, if you give up now you will be in pain forever</p>
-        <Link to="/signup"><img src={image2} alt="random image" className="mx-auto mt-16 mr-2" /></Link>
+        <p className="text-gray-400 mb-24 pb-2">Let’s keep burning to achieve your goals, it hurts only temporarily, if you give up now you will be in pain forever</p>
+        <Link to="/signup"><img src={image2} alt="random image" className=" mt-16 ml-72 fixed bottom-0 " /></Link>
     </>
   );
 }
